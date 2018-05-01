1524cd279ea14c603baa38f201ee8c495afd9b31
Lior Karamany <lior.karamany@gmail.com>
2018-05-01 14:01:28 +0300

    First Upload

---
+++ app/src/main/java/com/example/user/liork_tables/Students.java
@@ -0,0 +1,16 @@
+package com.example.user.liork_tables;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by User on 24/04/2018.
+ */
+
+public class Students {
+    private String login;
+    private String name;
+    private String lgroup;
+    private String phone;
+}
